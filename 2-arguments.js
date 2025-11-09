ea9997f8f53abdc8dfc6a43d78e74f586e3b9773
Patrick Brainoo <patrickbrainoo@gmail.com>
2025-11-09 14:07:34 -0800

    Add 2-arguments script with README

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,8 @@
 const args = ProcessingInstruction.argv.slice(2); // Get arguments passed after the script
+
 if (args.length === 0) {
     console.log("No argument");
-} else if (args.lenght === 1) {
+} else if (args.length === 1) {
     console.log("Argument found");
 } else {
     console.log("Arguments found");
