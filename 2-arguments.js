bab7ee9f3527e95995c7557f18aa001f2bdf766a
Patrick Brainoo <patrickbrainoo@gmail.com>
2025-11-09 14:03:47 -0800

    Add 2-arguments script with REAMDE

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 const args = ProcessingInstruction.argv.slice(2); // Get arguments passed after the script
 if (args.length === 0) {
     console.log("No argument");
-}else if (args.lenght === 1) {
+} else if (args.lenght === 1) {
     console.log("Argument found");
-}else{
+} else {
     console.log("Arguments found");
 }
